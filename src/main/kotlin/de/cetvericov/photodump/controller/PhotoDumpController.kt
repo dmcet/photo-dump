1354b974b43d9d98536aa0ab2db6506e373de418
Dmitrii Cetvericov <d.cetvericov@live.de>
2025-06-05 21:15:33 +0200

    clean things up a bit

--- src/main/kotlin/de/cetvericov/photodump/controller/PhotoDumpController.kt
+++ src/main/kotlin/de/cetvericov/photodump/controller/PhotoDumpController.kt
@@ -1,18 +1,11 @@
 package de.cetvericov.photodump.controller
 
 import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class PhotoDumpController {
-    @GetMapping("/greeting")
+    @GetMapping("/upload")
     fun greeting(
-        @RequestParam(value = "name", required = false, defaultValue = "World") name: String,
-        model: Model
-    ): String {
-        model.addAttribute("name", name)
-        return "greeting"
-    }
+    ): String = "redirect:/upload.html"
 }
