6fd7d17689ae793cecd86bb5a7c7a9e670fb10a3
Dmitrii Cetvericov <d.cetvericov@live.de>
2025-06-11 08:54:28 +0200

    allow cross origin requests

--- src/main/kotlin/de/cetvericov/photodump/api/controller/ImagesController.kt
+++ src/main/kotlin/de/cetvericov/photodump/api/controller/ImagesController.kt
@@ -11,23 +11,15 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.http.codec.multipart.FilePart
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestPart
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.net.URI
 
 @RestController
 @RequestMapping("/api/v1/images")
+@CrossOrigin(origins = ["http://localhost:3000"])
 class ImagesController(private val imageRepository: ImageRepository) {
-
     @GetMapping
     fun getImages(): Flow<ImageDto> = imageRepository.findAll().map(ImageDto::fromEntity).asFlow()
-
 
     @GetMapping("/{id}")
     suspend fun getImageData(@PathVariable id: Long): ByteArray? {
