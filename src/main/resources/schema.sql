55988dcec71ddbc12419117b6341c8ab8dacc63c
Dmitrii Cetvericov <d.cetvericov@live.de>
2025-06-26 19:55:44 +0200

    Pimp schema.sql but it works

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    id         SERIAL PRIMARY KEY,
+    id         BIGSERIAL PRIMARY KEY,
     username   VARCHAR(255) NOT NULL UNIQUE,
     password   VARCHAR(500) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS images
@@ -12,3 +12,5 @@ CREATE TABLE IF NOT EXISTS images
     name     VARCHAR(255)                                    NOT NULL,
     owner_id INTEGER REFERENCES users (id) ON DELETE CASCADE NOT NULL
 );
+
+CREATE INDEX IF NOT EXISTS idx_images_owner_id ON images (owner_id)
